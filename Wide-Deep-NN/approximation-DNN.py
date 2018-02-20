2277770b9c85170cfb9792cc061a9f207fcb2c1e
dongpilYu <yoodongphil@naver.com>
2018-02-20 13:46:21 +0900

    Delete approximation-DNN.py

--- Wide-Deep-NN/approximation-DNN.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import argparse
-import shutil
-import sys
-import tensorflow as tf
-
